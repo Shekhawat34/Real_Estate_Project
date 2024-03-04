e6864b3ad90437d0c78659cdb263be1854bad88d
lokender singh shekhawat <lokendershekhawat092@gmail.com>
2024-03-04 11:58:35 +0530

    updated the github link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer=()=>(
     <Box textAlign="center" p="S" color="grey.600" borderTop="1px" borderColor="grey.100">
         2024 Realtor,  Inc. All rights reserved. ©<br />
        <Icon>
-            <a href="https://github.com/Lokender98"><BsGithub size={20}/>Github</a>
+            <a href="https://github.com/Shekhawat34"><BsGithub size={20}/>Github</a>
            
         </Icon>
         Made with love by{" Lokender Singh Shekhawat"}
